dde11e899d6c67f57007a7bbb08bbf207e57a462
paioneer64 <om3095019@gmail.com>
2024-10-29 19:38:39 +0300

    Add files via upload

---
+++ Woker1.py
@@ -0,0 +1,13 @@
+# "tst program"
+print(9**0.5*5)
+
+# "2 end program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#  "3rd program
+print ((2 * 2 +2) == (2 * (2 + 2)))
+
+# 4th program
+print((int((123.456)*10 % (10))))
+
+
